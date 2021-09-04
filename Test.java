4dfc7a26c6dc7b980e9714c948d1fce5a1147199
asithan42 <asithan42@gmail.com>
2021-09-04 09:22:54 +0530

    updated

--- Test.java
+++ Test.java
@@ -5,17 +5,13 @@
  */
 package test;
 
-/**
- *
- * @author asitha
- */
+
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello");
+        
     }
     
 }
